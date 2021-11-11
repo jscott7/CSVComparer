3a6833854153a1075db6a752e032f686deef639f
Jonathan Scott <jonathanscott80@virginmedia.com>
2021-11-11 19:07:48 +0000

    Add break reporting test

---
+++ CSVComparisonTests/TestReporting.cs
@@ -0,0 +1,15 @@
+﻿using CSVComparison;
+using NUnit.Framework;
+
+namespace CSVComparisonTests
+{
+    public class TestReporting
+    {
+        [Test]
+        public void TestBreakDetailOutput()
+        {
+            var breakDetail = new BreakDetail() { BreakDescription = "A break", BreakType=BreakType.ValueMismatch, Column = "COL1"};
+            Assert.AreEqual("Break Type: ValueMismatch. Description A break", breakDetail.ToString());
+        }
+    }
+}
