7f1c9e25311a694479ae41742fe7a9016ce56ac4
Jonathan Scott <jonathanscott80@virginmedia.com>
2022-01-25 20:34:42 +0000

    Update to .NET 6

---
+++ CSVComparisonTests/TestComparisonUtils.cs
@@ -0,0 +1,20 @@
+﻿using CSVComparison;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSVComparisonTests
+{
+    public class TestComparisonUtils
+    {
+        [Test]
+        [Ignore("Test not yet fully implemented")]
+        public void TestSingleFileComparison()
+        {       
+            Console.WriteLine($"{Directory.GetCurrentDirectory()} - {Path.GetTempPath()} - {Path.GetTempFileName()}"); // + TestData
+          //  ComparisonUtils.RunSingleComparison(configurationFilePath, referenceFilePath, outputFilePath);
+        }
+    }
+}
