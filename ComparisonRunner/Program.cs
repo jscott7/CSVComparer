107b8eccb649734ca2dae9d0129a2ad0374b0839
Jonathan Scott <jonathanscott80@virginmedia.com>
2022-02-28 20:52:37 +0000

    Update to .NET 6.0 console template

--- ComparisonRunner/Program.cs
+++ ComparisonRunner/Program.cs
@@ -1,50 +1,40 @@
-﻿using System;
-using System.IO;
+﻿using CSVComparison;
 
-namespace CSVComparison
+if (args.Length < 3)
 {
-    class Program
+    Console.WriteLine($"Invalid number of arguments {args.Length}. Expecting at least 3");
+    Console.WriteLine("Arguments used:");
+    foreach(string arg in args)
     {
-        static void Main(string[] args)
-        {
-            if (args.Length < 3)
-            {
-                Console.WriteLine($"Invalid number of arguments {args.Length}. Expecting at least 3");
-                Console.WriteLine("Arguments used:");
-                foreach(string arg in args)
-                {
-                    Console.WriteLine(arg);
-                }
-
-                Console.WriteLine("Usage: CsVComparison ReferenceFilePath CandidateFilePath ConfigurationFilePath [PathToOutputFile]");
-                return;
-            }
-
-            var referenceFilePath = args[0];
-            var candidateFilePath = args[1]; 
-            var configurationFilePath = args[2];
-
-            string outputFilePath = "";
-            if (args.Length > 3)
-            {
-                outputFilePath = args[3];
-                if (!Directory.Exists(outputFilePath))
-                {
-                    Console.WriteLine($"Creating {outputFilePath} to save results");
-                    Directory.CreateDirectory(outputFilePath);
-                }
-            }
-    
-            if (Directory.Exists(referenceFilePath))
-            {
-                // This is a directory
-                ComparisonUtils.RunDirectoryComparison(configurationFilePath, referenceFilePath, candidateFilePath, outputFilePath);
-            }
-            else
-            {
-                // Default to single file comparison
-                ComparisonUtils.RunSingleComparison(configurationFilePath, referenceFilePath, candidateFilePath, outputFilePath);
-            }
-        }
+        Console.WriteLine(arg);
     }
+
+    Console.WriteLine("Usage: CsVComparison ReferenceFilePath CandidateFilePath ConfigurationFilePath [PathToOutputFile]");
+    return;
+}
+
+var referenceFilePath = args[0];
+var candidateFilePath = args[1]; 
+var configurationFilePath = args[2];
+
+string outputFilePath = "";
+if (args.Length > 3)
+{
+    outputFilePath = args[3];
+    if (!Directory.Exists(outputFilePath))
+    {
+        Console.WriteLine($"Creating {outputFilePath} to save results");
+        Directory.CreateDirectory(outputFilePath);
+    }
+}
+    
+if (Directory.Exists(referenceFilePath))
+{
+    // This is a directory
+    ComparisonUtils.RunDirectoryComparison(configurationFilePath, referenceFilePath, candidateFilePath, outputFilePath);
+}
+else
+{
+    // Default to single file comparison
+    ComparisonUtils.RunSingleComparison(configurationFilePath, referenceFilePath, candidateFilePath, outputFilePath);
 }
